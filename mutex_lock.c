eadd81db08183693e9fd31a3dfc14c2f2e89b865
Ricci <atmasdf@gmail.com>
2017-01-31 12:14:01 +0900

    lock & unlock

--- mutex_lock.c
+++ mutex_lock.c
@@ -4,12 +4,15 @@
 #include <pthread.h>
 
 int counter = 0;
+pthread_mutex_t lock;
 #define NUM_THREAD 10000
 void * thr_main(void *arg)
 {
 	//printf("thread %d start\n", (int)(intptr_t)arg);
+	pthread_mutex_lock(&lock);
 	counter++;
-	sleep(1);
+	//sleep(1);
+	pthread_mutex_unlock(&lock);
 	return ((void*)1);
 }
 
